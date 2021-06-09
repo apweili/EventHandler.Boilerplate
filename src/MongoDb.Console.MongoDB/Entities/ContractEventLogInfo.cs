c9007098289899090768061fb81ce1a67d0673ea
weili <woshilw0416@gmail.com>
2021-06-09 18:50:43 +0800

    modify event contractInfo

--- src/MongoDb.Console.MongoDB/Entities/ContractEventLogInfo.cs
+++ src/MongoDb.Console.MongoDB/Entities/ContractEventLogInfo.cs
@@ -4,16 +4,22 @@ using Volo.Abp.Domain.Entities;
 
 namespace MongoDb.Console.MongoDB.Entities
 {
-    public class ContractEventLogInfo: Entity<Guid>
+    public class ContractEventLogInfo : IEntity<string>
     {
-        public string EventId { get; set; }
-        
+        public string Id { get; }
+        public ContractEventLogInfo(string eventId)
+        {
+            Id = eventId;
+        }
         public string Token { get; set; }
-        
         public BigInteger LastLogBlock { get; set; }
-        
         public string ContractAddress { get; set; }
-        
+        public string NodeName { get; set; }
+        public BigInteger Timestamp { get; set; }
         public string EventName { get; set; }
+        public object[] GetKeys()
+        {
+            return new object[] {Id};
+        }
     }
 }
